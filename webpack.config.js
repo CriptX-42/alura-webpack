4177ae4f1aa24e50a3c09083115da2e01724fd2f
CriptX-42 <ricardo-svc@live.com>
2021-10-13 22:08:02 -0300

    Instalando o bootstrap como uma denpendencia, injetando ele ao bundle do. WIP: colocar o boostrap como estilo ao produto do projeto

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path')
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: './app/src/js/app.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'app/dist')
+    path: path.resolve(__dirname, 'app/dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['css-loader']
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './app/src/app.html',
       filename: 'app.html',
-      clean: true,
       hash: true
-    }),
-    new CopyWebpackPlugin({
-      patterns: [{ from: './app/src/css', to: 'css' }]
     })
   ]
 }
